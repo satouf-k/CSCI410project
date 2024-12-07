2b63aacd874140e9739d57bc2ebdbd33d4c57072
satouf-k <SatoufKebtawi@gmail.com>
2024-12-07 22:12:29 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+void main() {
+  runApp(MaterialApp(
+   
+    title: 'CSCI410 Unit Conversion',
+    debugShowCheckedModeBanner: false,
+     home: MeasureConverter(),
+  ));
+}
+
